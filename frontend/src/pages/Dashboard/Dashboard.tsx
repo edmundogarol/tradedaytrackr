19bc6dd1097e36ac55d27a748505fcb43dca812b
Edmundo Garol <egarol@outlook.com>
2026-01-15 05:38:53 +0800

    Started adding Funding Overview sections

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -5,12 +5,28 @@ import CalendarSummary, {
   CalendarSummaryCount,
 } from "@components/Calendar/CalendarSummary/CalendarSummary";
 import StatsSummary from "@components/Stats/StatsSummary/StatsSummary";
+import AssuredWorkloadIcon from "@mui/icons-material/AssuredWorkload";
+import StickyNote2Icon from "@mui/icons-material/StickyNote2";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import HardwareIcon from "@mui/icons-material/Hardware";
+
+import { GlowingIconWrapper } from "@components/GlassTile/GlassTileStyledComponents";
 import {
   DashboardContainer,
   DashboardContentCalendarContainer,
   DashboardContentHeader,
   DashboardContentJournalEntryContainer,
   DashboardContentOtherContainer,
+  DashboardTopSectionContainer,
+  DashboardTopSectionLeftContainer,
+  DashboardTopSectionRightContainer,
+  FundingOverviewSection,
+  FundingOverviewSectionContent,
+  FundingOverviewSectionContentValue,
+  FundingOverviewSectionContentValueContainer,
+  FundingOverviewSectionContentValueHighlighted,
+  FundingOverviewSectionTitle,
+  FundingOverviewTitle,
 } from "./DashboardStyledComponents";
 import UpcomingPayoutDetails from "./UpcomingPayoutDetails/UpcomingPayoutDetails";
 
@@ -19,14 +35,93 @@ const Dashboard: React.FunctionComponent = () => {
     <Page topBarShowMenu={true}>
       <DashboardContainer>
         <Gap level={4} />
-        <DashboardContentJournalEntryContainer>
-          <UpcomingPayoutDetails />
-        </DashboardContentJournalEntryContainer>
-        <Gap level={2} />
-        <DashboardContentJournalEntryContainer>
-          <DashboardContentHeader>Current Stats</DashboardContentHeader>
-          <StatsSummary />
-        </DashboardContentJournalEntryContainer>
+        <DashboardTopSectionContainer>
+          <DashboardTopSectionLeftContainer>
+            <DashboardContentJournalEntryContainer>
+              <UpcomingPayoutDetails />
+            </DashboardContentJournalEntryContainer>
+            <Gap level={2} />
+            <DashboardContentJournalEntryContainer>
+              <DashboardContentHeader>Current Stats</DashboardContentHeader>
+              <StatsSummary />
+            </DashboardContentJournalEntryContainer>
+          </DashboardTopSectionLeftContainer>
+          <DashboardTopSectionRightContainer>
+            <FundingOverviewTitle>Funding Overview</FundingOverviewTitle>
+            <FundingOverviewSection>
+              <FundingOverviewSectionTitle>
+                Total Active Funding
+              </FundingOverviewSectionTitle>
+              <FundingOverviewSectionContent>
+                <GlowingIconWrapper $positive $size={30}>
+                  <AssuredWorkloadIcon style={{ color: "#95d395" }} />
+                </GlowingIconWrapper>
+                <FundingOverviewSectionContentValueContainer>
+                  <FundingOverviewSectionContentValueHighlighted>
+                    ${"250,000"}
+                  </FundingOverviewSectionContentValueHighlighted>
+                  <FundingOverviewSectionContentValue>
+                    {"2 x Apex, 2 x MFFU, 2 x Bulenox"}
+                  </FundingOverviewSectionContentValue>
+                </FundingOverviewSectionContentValueContainer>
+                <ChevronRightIcon
+                  style={{ color: "#d1d1d1", fontSize: 30, marginLeft: "auto" }}
+                  onClick={() =>
+                    alert("expand or navigate to funded accounts details")
+                  }
+                />
+              </FundingOverviewSectionContent>
+            </FundingOverviewSection>
+            <FundingOverviewSection>
+              <FundingOverviewSectionTitle>
+                Evaluations Passed
+              </FundingOverviewSectionTitle>
+              <FundingOverviewSectionContent>
+                <GlowingIconWrapper $positive $size={30}>
+                  <StickyNote2Icon style={{ color: "#95d395" }} />
+                </GlowingIconWrapper>
+                <FundingOverviewSectionContentValueContainer>
+                  <FundingOverviewSectionContentValueHighlighted>
+                    {"3 / 5"}
+                  </FundingOverviewSectionContentValueHighlighted>
+                  <FundingOverviewSectionContentValue>
+                    {"View eval accounts"}
+                  </FundingOverviewSectionContentValue>
+                </FundingOverviewSectionContentValueContainer>
+                <ChevronRightIcon
+                  style={{ color: "#d1d1d1", fontSize: 30, marginLeft: "auto" }}
+                  onClick={() =>
+                    alert("expand or navigate to eval accounts details")
+                  }
+                />
+              </FundingOverviewSectionContent>
+            </FundingOverviewSection>
+            <FundingOverviewSection>
+              <FundingOverviewSectionTitle>
+                Buffer Progress
+              </FundingOverviewSectionTitle>
+              <FundingOverviewSectionContent>
+                <GlowingIconWrapper $positive $size={30}>
+                  <HardwareIcon style={{ color: "#95d395" }} />
+                </GlowingIconWrapper>
+                <FundingOverviewSectionContentValueContainer>
+                  <FundingOverviewSectionContentValueHighlighted>
+                    ${"320 / $2100"}
+                  </FundingOverviewSectionContentValueHighlighted>
+                  <FundingOverviewSectionContentValue>
+                    {"On 3 Apex Funded Accounts"}
+                  </FundingOverviewSectionContentValue>
+                </FundingOverviewSectionContentValueContainer>
+                <ChevronRightIcon
+                  style={{ color: "#d1d1d1", fontSize: 30, marginLeft: "auto" }}
+                  onClick={() =>
+                    alert("expand or navigate to eval accounts details")
+                  }
+                />
+              </FundingOverviewSectionContent>
+            </FundingOverviewSection>
+          </DashboardTopSectionRightContainer>
+        </DashboardTopSectionContainer>
         <Gap level={2} />
         <DashboardContentCalendarContainer>
           <DashboardContentHeader>Recent Activity</DashboardContentHeader>
--- frontend/src/pages/Dashboard/DashboardStyledComponents.tsx
+++ frontend/src/pages/Dashboard/DashboardStyledComponents.tsx
@@ -1,3 +1,4 @@
+import { CONTAINER_PADDING_DEFAULT } from "@styles/constants";
 import styled from "styled-components";
 
 export const DashboardContainer = styled.div`
@@ -25,3 +26,83 @@ export const DashboardContentHeader = styled.h1`
 export const DashboardContentJournalEntryContainer = styled.div``;
 
 export const DashboardContentOtherContainer = styled.div``;
+
+export const DashboardTopSectionContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
+`;
+
+export const DashboardTopSectionLeftContainer = styled.div`
+  height: max-content;
+  flex: 2 1 auto;
+`;
+
+export const DashboardTopSectionRightContainer = styled.div`
+  width: 300px;
+  min-width: 300px;
+  background: #ac4242;
+  flex: 1;
+  background: #ffffff0f;
+  border-radius: 5px;
+  padding: ${CONTAINER_PADDING_DEFAULT}px;
+`;
+
+export const FundingOverviewTitle = styled.h2`
+  margin: 10px;
+  color: #d2d2d2;
+  font-size: 16px;
+  align-items: center;
+  display: flex;
+  text-shadow: 1px 1px 2px black;
+  white-space: nowrap;
+  flex-wrap: wrap;
+`;
+
+export const FundingOverviewSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  white-space: nowrap;
+  padding: 4px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border-radius: 11px;
+  border: 1px solid #4a5568;
+  background: #a1a1a112;
+  margin-bottom: 10px;
+`;
+
+export const FundingOverviewSectionTitle = styled.h3`
+  margin: 10px;
+  margin-left: 0;
+  margin-bottom: 5px;
+  color: #d2d2d2;
+  font-size: 14px;
+  align-items: center;
+  display: flex;
+  text-shadow: 1px 1px 2px black;
+  white-space: nowrap;
+  flex-wrap: wrap;
+`;
+
+export const FundingOverviewSectionContent = styled.div`
+  align-items: flex-start;
+  display: flex;
+`;
+
+export const FundingOverviewSectionContentValueContainer = styled.div`
+  margin-left: 7px;
+`;
+
+export const FundingOverviewSectionContentValue = styled.span`
+  color: #c6c6c6;
+  font-weight: 100;
+  margin-left: 10px;
+  white-space: normal;
+  font-size: 14px;
+`;
+
+export const FundingOverviewSectionContentValueHighlighted = styled.span`
+  color: #95d395;
+  font-size: 20px;
+`;
