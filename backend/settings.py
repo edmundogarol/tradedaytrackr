1412c4d41ac729b6faa641e4343a450bf6dd7932
Edmundo Garol <egarol@outlook.com>
2026-04-03 06:20:29 +0800

    Update cors and hosts settings

--- backend/settings.py
+++ backend/settings.py
@@ -36,10 +36,14 @@ if "DEVENV" in os.environ:
     ALLOWED_HOSTS = ["*"]
 else:
     ALLOWED_HOSTS = [
-        "*",
         "api.tradedaytrackr.com",
         "tradedaytrackr.com",
     ]
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SAMESITE = "None"
+CSRF_COOKIE_SAMESITE = "None"
 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
