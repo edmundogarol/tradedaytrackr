c9edfbdb1dca43f1c624a1869f780d42ff02b115
Edmundo Garol <egarol@outlook.com>
2026-04-02 08:43:28 +0800

    Update env accessor

--- frontend/src/utils/environmentConfig.ts
+++ frontend/src/utils/environmentConfig.ts
@@ -1,13 +1,13 @@
 //@ts-ignore
-// import { HOST } from "@env";
+import { HOST } from "@env";
 
 export interface EnvironmentConfig {
   HOST?: string;
 }
 
-// console.log({ HOST });
+console.log({ HOST });
 const environmentConfig: EnvironmentConfig = {
-  HOST: "http://localhost:8000",
+  HOST: process.env.REACT_APP_API_URL || "http://localhost:8000",
 };
 
 export default environmentConfig;
