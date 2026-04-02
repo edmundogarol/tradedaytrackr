df6bdd26b57dec5bdcfc1b57b8edbb5db080f74e
Edmundo Garol <egarol@outlook.com>
2026-04-02 08:45:01 +0800

    remove env

--- frontend/src/utils/environmentConfig.ts
+++ frontend/src/utils/environmentConfig.ts
@@ -1,11 +1,9 @@
 //@ts-ignore
-import { HOST } from "@env";
 
 export interface EnvironmentConfig {
   HOST?: string;
 }
 
-console.log({ HOST });
 const environmentConfig: EnvironmentConfig = {
   HOST: process.env.REACT_APP_API_URL || "http://localhost:8000",
 };
