24b7395f759e2822635d8815a926c97b0bbfb861
Edmundo Garol <egarol@outlook.com>
2026-04-20 23:28:25 +0800

    Admin updated to see user data

--- backend/djangoapi/admin.py
+++ backend/djangoapi/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 
 from backend.djangoapi.models.rule import Rule
+from backend.djangoapi.models.user import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "last_ip",
+        "is_verified",
+        "date_joined",
+    ]
+    search_fields = ["email", "first_name", "last_name"]
 
 
 @admin.register(Rule)
