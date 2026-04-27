16240bae0cecd00dc2e5f6e58349c555a40cdf76
Edmundo Garol <egarol@outlook.com>
2026-04-27 18:14:52 +0800

    Add social links

--- frontend/src/components/BottomBar/BottomBar.tsx
+++ frontend/src/components/BottomBar/BottomBar.tsx
@@ -1,7 +1,10 @@
 import { PageEnum } from "@interfaces/NavigationTypes";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
 import React from "react";
 import { Container, BottomBarLink as Link } from "./BottomBarStyledComponents";
-
 export interface BottomBarProps {
   transparent?: boolean;
 }
@@ -20,14 +23,41 @@ const BottomBar: React.FunctionComponent<BottomBarProps> = ({
       >
         © 2026 TradeDayTrackR. All rights reserved.
       </span>
-      <div>
+      <div style={{ display: "flex", alignItems: "center" }}>
         <Link to={PageEnum.PrivacyPolicy}>Privacy Policy</Link>
         <Link to={PageEnum.TermsOfService}>Terms of Service</Link>
         <Link to={PageEnum.FrequentlyAskedQuestions}>FAQ</Link>
         <Link to={PageEnum.ContactUs}>Contact Us</Link>
+        <Link
+          to="https://www.instagram.com/tradedaytrackr/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </Link>
+        <Link
+          to="https://www.facebook.com/tradedaytrackr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon />
+        </Link>
+        <Link
+          to="https://www.x.com/tradedaytrackr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <XIcon />
+        </Link>
+        <Link
+          to="https://www.threads.com/@tradedaytrackr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <AlternateEmailIcon />
+        </Link>
       </div>
     </Container>
   );
 };
-
 export default BottomBar;
